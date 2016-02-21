b3b181898c8d8ad908e1e88112aacb3a07e63872
Parag Dadhaniya <paragdadhaniya@gmail.com>
2016-02-21 12:36:10 -0600

    Refactor some more

--- app/controllers/rsvp_controller.rb
+++ app/controllers/rsvp_controller.rb
@@ -6,32 +6,19 @@ class RsvpController < ApplicationController
   def show
     name = params.fetch('name')
     zip  = params.fetch('zip')
+
     guest = Guest.where(full_name: name).where(zip: zip)
+
     if guest.count == 0
       # TODO handle errors
-    end
-    if guest.count == 1
-      family = Family.find(guest.first.family_id)
-      guests = family.guests
-    end
-
-    if guest.count > 1
-      # TODO show table of options
+    elsif guest.count == 1
+      guests = get_guests_for_family(guest)
+    else
+      # TODO show table of options for multiple guests
     end
 
-    events = Event.all
-    @events_hash = {}
-    events.map { |event| @events_hash[event.id] = event.event_name }
-
-    @guest_data = guests.map do |guest|
-      rsvps = Rsvp.where(guest_id: guest.id)
-      {
-        "id" => guest.id,
-        "full_name" => guest.full_name,
-        "email" => guest.email,
-        "rsvps" => rsvps
-      }
-    end
+    @events_hash = make_events_hash
+    @guest_data = hydrate_guest_data(guests)
   end
 
   def update
@@ -45,5 +32,31 @@ class RsvpController < ApplicationController
       rsvp.status = rsvp_hash[rsvp_id]
       rsvp.save!
     end
+  end
+
+  private
+
+  def make_events_hash
+    events = Event.all
+    events_hash = {}
+    events.map { |event| events_hash[event.id] = event.event_name }
+    return events_hash
+  end
+
+  def hydrate_guest_data(guests)
+    guests.map do |guest|
+      rsvps = Rsvp.where(guest_id: guest.id)
+      {
+        "id" => guest.id,
+        "full_name" => guest.full_name,
+        "email" => guest.email,
+        "rsvps" => rsvps
+      }
+    end
+  end
+
+  def get_guests_for_family(guest)
+    family = Family.includes(:guests).find(guest.first.family_id)
+    family.guests
   end
 end

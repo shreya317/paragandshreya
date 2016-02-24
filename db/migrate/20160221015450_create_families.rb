8ddc59174f990b11eccf9434cb87ded5e08f9706
Shreya Patel <shreya317@gmail.com>
2016-02-23 18:36:46 -0600

    added message to Family model

--- db/migrate/20160221015450_create_families.rb
+++ db/migrate/20160221015450_create_families.rb
@@ -2,6 +2,7 @@ class CreateFamilies < ActiveRecord::Migration
   def change
     create_table :families do |t|
       t.string :family_name, null: false
+      t.string :message
 
       t.timestamps null: false
     end

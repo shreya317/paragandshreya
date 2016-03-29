0d1d175e74c28b19af13bb0d7742f06205b4f675
Parag Dadhaniya <paragdadhaniya@gmail.com>
2016-03-28 20:12:01 -0500

    Updates

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,21 +1,19 @@
 class EventsController < ApplicationController
   http_basic_authenticate_with name: "shreya", password: "shreyag", except: :index
 
-
   def index
   end
 
   def search
     @event_list = Event.all
 
-  	if (params[:event])
-  		event_name = Event.find_by(event_name: params[:event])
-			@event_rsvps = @event_name.rsvps.order(status: :desc)			
-			
-			@attending = @event_name.rsvps.where(status: "Yes").length
-			@not_attending = @event_name.rsvps.where(status: "No").length
-			@no_reply = @event_name.rsvps.where(status: "--").length
-		end
+    if (params[:event])
+      event_name     = Event.find_by(event_name: params[:event])
+      @event_rsvps   = event_name.rsvps.order(status: :desc)
+      @attending     = event_name.rsvps.where(status: "Yes").length
+      @not_attending = event_name.rsvps.where(status: "No").length
+      @no_reply      = event_name.rsvps.where(status: "--").length
+    end
   end
 
 end
